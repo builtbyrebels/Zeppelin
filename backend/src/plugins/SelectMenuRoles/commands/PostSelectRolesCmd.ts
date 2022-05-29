0abea6f179838ec39f3012b184cc64d91a0958e2
perpelator <tgamer7@outlook.com>
2022-05-28 21:42:47 -0700

    reeeee

--- backend/src/plugins/SelectMenuRoles/commands/PostSelectRolesCmd.ts
+++ backend/src/plugins/SelectMenuRoles/commands/PostSelectRolesCmd.ts
@@ -56,7 +56,8 @@ export const PostSelectRolesCmd = selectRolesCmd({
     const rows = splitButtonsIntoRows(selectMenus, Object.values(group.menus)); // new MessageActionRow().addComponents(buttons);
 
     try {
-      const newMsg = await args.channel.send({content: group.message as string | MessageOptions, components: rows });
+      const newMsg = await args.channel.send(group.message as string | MessageOptions );
+      await group.message.edit({ components: rows });
 
       for (const btn of toInsert) {
         await pluginData.state.selectMenus.add(args.channel.id, newMsg.id, btn.customId, btn.selectGroup, btn.menuName);
